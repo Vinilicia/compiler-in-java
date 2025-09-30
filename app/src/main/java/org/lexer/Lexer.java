9d9f158389d18e407aa3cc0891b09f66b8c301f8
davijannotti <davi29012004@gmail.com>
2025-09-29 21:25:49 -0300

    save state for future recovery

--- app/src/main/java/org/lexer/Lexer.java
+++ app/src/main/java/org/lexer/Lexer.java
@@ -9,18 +9,211 @@ public class Lexer {
 
     private final List<Character> characters;
 
+    ReservedWords reservedWords = new ReservedWords();
+
     public Lexer(List<Character> characters) {
         this.characters = characters;
     }
 
     public List<Token> tokenize() {
         List<Token> tokens = new ArrayList<>();
-
+        String lexeme = new String();
         int lineNumber = 1;
         int state = 0;
+        boolean isLexemeOver = false;
+        boolean isLastCharacterUsed = true;
+        TokenType actualTokenType = null;
+        int i = 0;
+        
+        while(i < characters.size()) {
+            isLastCharacterUsed = true;
+            char c = characters.get(i);
+            switch (state) {
+                case 0:
+                    switch(c) {
+                        case '\n':
+                            lexeme = "";
+                            lineNumber++;
+                            break;
+                        
+                        case ' ':
+                            lexeme = "";
+                            break;
 
-        for (char c : characters) {
-            System.out.println(c);
+                        case '(':
+                            actualTokenType = TokenType.LBRACE;
+                            isLexemeOver = true;
+                        break;
+
+                        case ')':
+                            actualTokenType = TokenType.RBRACE;
+                            isLexemeOver = true;
+                        break;
+
+                        case '{':
+                            actualTokenType = TokenType.LBRACKET;
+                            isLexemeOver = true;
+                        break;
+
+                        case '}':
+                            actualTokenType = TokenType.RBRACKET;
+                            isLexemeOver = true;
+                        break;
+
+                        case ':':
+                            actualTokenType = TokenType.COLON;
+                            isLexemeOver = true;
+                        break;
+
+                        case ';':
+                            actualTokenType = TokenType.SEMICOLON;
+                            isLexemeOver = true;
+                        break;
+
+                        case ',':
+                            actualTokenType = TokenType.COMMA;
+                            isLexemeOver = true;
+                        break;
+
+                        case '+':
+                            actualTokenType = TokenType.PLUS;
+                            isLexemeOver = true;
+                        break;
+
+                        case '*':
+                            actualTokenType = TokenType.MULT;
+                            isLexemeOver = true;
+                        break;
+
+                        case '/':
+                            actualTokenType = TokenType.DIV;
+                            isLexemeOver = true;
+                        break;
+                        
+                        case '-':
+                            state = 1;
+                        break;
+                        
+                        case '!':
+                            state = 2;
+                        break;
+                        
+                        case '=':
+                            state = 3;
+                        break;
+                        
+                        case '>':
+                            state = 4;
+                        break;
+                        
+                        default:
+                            if (Character.isLetter(c)) {
+                                state = 6;
+                            } else if (Character.isDigit(c)) {
+                                state = 7;
+                            }
+                        break;
+                    }
+                    break;
+                    
+                case 1:
+                    if(c == '>'){
+                        actualTokenType = TokenType.ARROW;
+                    } else {
+                        actualTokenType = TokenType.MINUS;
+                        isLastCharacterUsed = false;
+                    }   
+                    isLexemeOver = true;
+                    state = 0;
+                break;
+                
+                case 2:
+                    if(c == '='){
+                        actualTokenType = TokenType.NE;
+                        isLexemeOver = true;
+                        state = 0;
+                    } else {
+                        System.out.println("Erro na linha " + lineNumber);
+                    }
+                break;
+                
+                case 3:
+                    if(c == '='){
+                        actualTokenType = TokenType.EQ;
+                    } else {
+                        actualTokenType = TokenType.ASSIGN;
+                        isLastCharacterUsed = false;
+                    }
+                    isLexemeOver = true;
+                    state = 0;
+                break;
+ 
+                case 4:
+                    if(c == '='){
+                        actualTokenType = TokenType.GE;
+                    } else {
+                        actualTokenType = TokenType.GT;
+                        isLastCharacterUsed = false;
+                    }
+                    isLexemeOver = true;
+                    state = 0;
+                break;
+                
+                case 5:
+                    if(c == '='){
+                        actualTokenType = TokenType.LE;
+                    } else {
+                        actualTokenType = TokenType.LT;
+                        isLastCharacterUsed = false;
+                    }
+                    state = 0;
+                    isLexemeOver = true;
+                break;
+
+                case 6:
+                    if (Character.isLetterOrDigit(c) || c == '_') {
+                        // continue accumulating the identifier
+                        System.err.println("fudeu " + c);
+
+                    } else {
+                        var reservedMap = reservedWords.getReservedWords();
+                        if(!reservedMap.containsKey(lexeme)){
+                            actualTokenType = TokenType.ID;
+                            System.err.println("sou gay");
+                        }
+                        else{
+                            actualTokenType = reservedMap.get(lexeme);
+                            System.err.println("sou gay");
+                        }
+                        isLastCharacterUsed = false;
+                        state = 0;
+                        isLexemeOver = true;
+                    }
+                break;
+                
+                case 7:
+                    if (Character.isDigit(c)){
+                        // does nothing
+                    } else {
+                        
+                    }
+                    
+                break;
+                default:
+                break;
+            }
+
+            if(isLastCharacterUsed){
+                i++;
+                lexeme += c;
+            }
+
+            if(isLexemeOver){
+                Token newToken = new Token(lexeme, actualTokenType, lineNumber); 
+                tokens.add(newToken);
+                lexeme = "";
+                isLexemeOver = false;
+            }
         }
         return tokens;
     }
