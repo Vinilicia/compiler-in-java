99bbfac5a725cbf1e21f734dfb664719b05ed2af
Vinícius Souza <viniso1903@gmail.com>
2025-10-11 13:34:21 -0300

    ListaArgs2 identify COMMA not COLON

--- app/src/main/java/org/syntactic/Syntactic.java
+++ app/src/main/java/org/syntactic/Syntactic.java
@@ -12,23 +12,30 @@ public class Syntactic {
     private Token token;
     private int i;
 
-    public Syntactic(List<Token> tokens){
+    public Syntactic(List<Token> tokens) {
         this.tokens = tokens;
         this.token = tokens.get(0);
         this.i = 0;
-        this.syntacticErrors = new ArrayList<>();
+        //this.syntacticErrors = new ArrayList<>();
     }
 
-    public void PrintError(){
-        SyntacticError newError = new SyntacticError(token, token.getLineNumber());
-        System.err.println("Syntactic error. Token " + token.toString() + " not expected in input.");
+    public void PrintError() {
+        SyntacticError newError = new SyntacticError(
+            token,
+            token.getLineNumber()
+        );
+        System.err.println(
+            "Syntactic error. Token " +
+                token.toString() +
+                " not expected in input."
+        );
         syntacticErrors.add(newError);
         i++;
         if (i < tokens.size()) {
             token = tokens.get(i);
         }
     }
-    
+
     public boolean match(TokenType expectedType) {
         if (token.getType() == expectedType) {
             i++;
@@ -47,24 +54,23 @@ public class Syntactic {
         return syntacticErrors;
     }
 
-    private void Programa(){
-        if(token.getType() == TokenType.FUNCTION){
+    private void Programa() {
+        if (token.getType() == TokenType.FUNCTION) {
             Funcao();
-            FuncaoSeq();    
-        }
-        else{
+            FuncaoSeq();
+        } else {
             PrintError();
         }
     }
 
-    private void FuncaoSeq(){
-        if(token.getType() == TokenType.FUNCTION){
+    private void FuncaoSeq() {
+        if (token.getType() == TokenType.FUNCTION) {
             Funcao();
-            FuncaoSeq();    
+            FuncaoSeq();
         }
     }
-    
-    private void Funcao(){
+
+    private void Funcao() {
         match(TokenType.FUNCTION);
         NomeFuncao();
         match(TokenType.LBRACKET);
@@ -74,18 +80,18 @@ public class Syntactic {
         Bloco();
     }
 
-    private void NomeFuncao(){
-        if(token.getType() == TokenType.ID){
+    private void NomeFuncao() {
+        if (token.getType() == TokenType.ID) {
             match(TokenType.ID);
-        } else if(token.getType() == TokenType.MAIN){
+        } else if (token.getType() == TokenType.MAIN) {
             match(TokenType.MAIN);
-        } else{
+        } else {
             PrintError();
         }
     }
 
-    private void ListaParams(){
-        if(token.getType() == TokenType.ID){
+    private void ListaParams() {
+        if (token.getType() == TokenType.ID) {
             match(TokenType.ID);
             match(TokenType.COLON);
             Type();
@@ -93,8 +99,8 @@ public class Syntactic {
         }
     }
 
-    private void ListaParams2(){
-        if(token.getType() == TokenType.COMMA){
+    private void ListaParams2() {
+        if (token.getType() == TokenType.COMMA) {
             match(TokenType.COMMA);
             match(TokenType.ID);
             match(TokenType.COLON);
@@ -102,33 +108,37 @@ public class Syntactic {
             ListaParams2();
         }
     }
-    
-    private void TipoRetornoFuncao(){
-        if(token.getType() == TokenType.ARROW){
+
+    private void TipoRetornoFuncao() {
+        if (token.getType() == TokenType.ARROW) {
             match(TokenType.ARROW);
             Type();
         }
     }
 
-    private void Bloco(){
+    private void Bloco() {
         match(TokenType.LBRACE);
         Sequencia();
         match(TokenType.RBRACE);
     }
 
-    private void Sequencia(){
-        if(token.getType() == TokenType.LET){
+    private void Sequencia() {
+        if (token.getType() == TokenType.LET) {
             Declaracao();
             Sequencia();
-        } else if(token.getType() == TokenType.ID || token.getType() == TokenType.IF || 
-           token.getType() == TokenType.WHILE || token.getType() == TokenType.PRINTLN ||
-           token.getType() == TokenType.RETURN){
+        } else if (
+            token.getType() == TokenType.ID ||
+            token.getType() == TokenType.IF ||
+            token.getType() == TokenType.WHILE ||
+            token.getType() == TokenType.PRINTLN ||
+            token.getType() == TokenType.RETURN
+        ) {
             Comando();
-            Sequencia();    
+            Sequencia();
         }
     }
 
-    private void Declaracao(){
+    private void Declaracao() {
         match(TokenType.LET);
         VarList();
         match(TokenType.COLON);
@@ -136,30 +146,27 @@ public class Syntactic {
         match(TokenType.SEMICOLON);
     }
 
-    private void VarList(){
+    private void VarList() {
         match(TokenType.ID);
         VarList2();
     }
 
-    private void VarList2(){
-        if(token.getType() == TokenType.COMMA){
+    private void VarList2() {
+        if (token.getType() == TokenType.COMMA) {
             match(TokenType.COMMA);
             match(TokenType.ID);
             VarList2();
         }
     }
 
-    private void Type(){
-        if(token.getType() == TokenType.INT){
+    private void Type() {
+        if (token.getType() == TokenType.INT) {
             match(TokenType.INT);
-        }
-        else if( token.getType() == TokenType.FLOAT){
+        } else if (token.getType() == TokenType.FLOAT) {
             match(TokenType.FLOAT);
-        }
-        else if(token.getType() == TokenType.CHAR){
+        } else if (token.getType() == TokenType.CHAR) {
             match(TokenType.CHAR);
-        }
-        else{
+        } else {
             PrintError();
         }
     }
@@ -170,17 +177,14 @@ public class Syntactic {
                 match(TokenType.ID);
                 AtribuicaoOuChamada();
                 break;
-
             case IF:
                 ComandoSe();
                 break;
-
             case WHILE:
                 match(TokenType.WHILE);
                 Expr();
                 Bloco();
                 break;
-
             case PRINTLN:
                 match(TokenType.PRINTLN);
                 match(TokenType.LBRACE);
@@ -190,248 +194,250 @@ public class Syntactic {
                 match(TokenType.RBRACE);
                 match(TokenType.SEMICOLON);
                 break;
-
             case RETURN:
                 match(TokenType.RETURN);
                 Expr();
                 match(TokenType.SEMICOLON);
                 break;
-
             default:
                 PrintError();
                 break;
         }
     }
 
-    private void AtribuicaoOuChamada(){
-        if(token.getType() == TokenType.ASSIGN){
+    private void AtribuicaoOuChamada() {
+        if (token.getType() == TokenType.ASSIGN) {
             match(TokenType.ASSIGN);
             Expr();
             match(TokenType.SEMICOLON);
-        }
-        else if( token.getType() == TokenType.LBRACKET){
+        } else if (token.getType() == TokenType.LBRACKET) {
             match(TokenType.LBRACKET);
             ListaArgs();
             match(TokenType.RBRACKET);
             match(TokenType.SEMICOLON);
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void ComandoSe(){
-        if(token.getType() == TokenType.IF){
+    private void ComandoSe() {
+        if (token.getType() == TokenType.IF) {
             match(TokenType.IF);
             Expr();
             Bloco();
             ComandoSenao();
-        }
-        else if( token.getType() == TokenType.LBRACE){
+        } else if (token.getType() == TokenType.LBRACE) {
             match(TokenType.LBRACE);
             Bloco();
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void ComandoSenao(){
-        if(token.getType() == TokenType.ELSE){
+    private void ComandoSenao() {
+        if (token.getType() == TokenType.ELSE) {
             match(TokenType.ELSE);
             ComandoSe();
         }
     }
 
-    private void Expr(){
-        if(token.getType() == TokenType.ID || token.getType() == TokenType.INT_CONST || token.getType() == TokenType.FLOAT_CONST || token.getType() == TokenType.CHAR_LITERAL || token.getType() == TokenType.LBRACKET){
+    private void Expr() {
+        if (
+            token.getType() == TokenType.ID ||
+            token.getType() == TokenType.INT_CONST ||
+            token.getType() == TokenType.FLOAT_CONST ||
+            token.getType() == TokenType.CHAR_LITERAL ||
+            token.getType() == TokenType.LBRACKET
+        ) {
             Rel();
             ExprOpc();
-        }
-        else{
+        } else {
             PrintError();
         }
     }
-    
-    private void ExprOpc(){
-        if(token.getType() == TokenType.EQ){
+
+    private void ExprOpc() {
+        if (token.getType() == TokenType.EQ) {
             OpIgual();
             Rel();
             ExprOpc();
         }
     }
 
-    private void OpIgual(){
-        if(token.getType() == TokenType.EQ){
+    private void OpIgual() {
+        if (token.getType() == TokenType.EQ) {
             match(TokenType.EQ);
-        }
-        else if(token.getType() == TokenType.NE){
+        } else if (token.getType() == TokenType.NE) {
             match(TokenType.NE);
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void Rel(){
-        if(token.getType() == TokenType.ID || token.getType() == TokenType.INT_CONST || token.getType() == TokenType.FLOAT_CONST || token.getType() == TokenType.CHAR_LITERAL || token.getType() == TokenType.LBRACKET){
+    private void Rel() {
+        if (
+            token.getType() == TokenType.ID ||
+            token.getType() == TokenType.INT_CONST ||
+            token.getType() == TokenType.FLOAT_CONST ||
+            token.getType() == TokenType.CHAR_LITERAL ||
+            token.getType() == TokenType.LBRACKET
+        ) {
             Adicao();
             RelOpc();
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void RelOpc(){
-        if(token.getType() == TokenType.LT){
+    private void RelOpc() {
+        if (token.getType() == TokenType.LT) {
             OpRel();
             Adicao();
             RelOpc();
         }
     }
 
-
-    private void OpRel(){
-        if(token.getType() == TokenType.LT){
+    private void OpRel() {
+        if (token.getType() == TokenType.LT) {
             match(TokenType.LT);
-        }
-        else if(token.getType() == TokenType.LE){
+        } else if (token.getType() == TokenType.LE) {
             match(TokenType.LE);
-        }
-        else if(token.getType() == TokenType.GT){
+        } else if (token.getType() == TokenType.GT) {
             match(TokenType.GT);
-        }
-        else if(token.getType() == TokenType.GE){
+        } else if (token.getType() == TokenType.GE) {
             match(TokenType.GE);
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void Adicao(){
-        if(token.getType() == TokenType.ID || token.getType() == TokenType.INT_CONST || token.getType() == TokenType.FLOAT_CONST || token.getType() == TokenType.CHAR_LITERAL || token.getType() == TokenType.LBRACKET){
+    private void Adicao() {
+        if (
+            token.getType() == TokenType.ID ||
+            token.getType() == TokenType.INT_CONST ||
+            token.getType() == TokenType.FLOAT_CONST ||
+            token.getType() == TokenType.CHAR_LITERAL ||
+            token.getType() == TokenType.LBRACKET
+        ) {
             Termo();
             AdicaoOpc();
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void AdicaoOpc(){
-        if(token.getType() == TokenType.PLUS){
+    private void AdicaoOpc() {
+        if (token.getType() == TokenType.PLUS) {
             OpAdicao();
             Termo();
             AdicaoOpc();
         }
     }
 
-    private void OpAdicao(){
-        if(token.getType() == TokenType.PLUS){
+    private void OpAdicao() {
+        if (token.getType() == TokenType.PLUS) {
             match(TokenType.PLUS);
-        }
-        else if(token.getType() == TokenType.MINUS){
+        } else if (token.getType() == TokenType.MINUS) {
             match(TokenType.MINUS);
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void Termo(){
-        if(token.getType() == TokenType.ID || token.getType() == TokenType.INT_CONST || token.getType() == TokenType.FLOAT_CONST || token.getType() == TokenType.CHAR_LITERAL || token.getType() == TokenType.LBRACKET){
+    private void Termo() {
+        if (
+            token.getType() == TokenType.ID ||
+            token.getType() == TokenType.INT_CONST ||
+            token.getType() == TokenType.FLOAT_CONST ||
+            token.getType() == TokenType.CHAR_LITERAL ||
+            token.getType() == TokenType.LBRACKET
+        ) {
             Fator();
             TermoOpc();
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void TermoOpc(){
-        if(token.getType() == TokenType.MULT || token.getType() == TokenType.DIV){
+    private void TermoOpc() {
+        if (
+            token.getType() == TokenType.MULT ||
+            token.getType() == TokenType.DIV
+        ) {
             OpMult();
             Fator();
             TermoOpc();
         }
     }
 
-    private void OpMult(){
-        if(token.getType() == TokenType.MULT){
+    private void OpMult() {
+        if (token.getType() == TokenType.MULT) {
             match(TokenType.MULT);
-        }
-        else if(token.getType() == TokenType.DIV){
+        } else if (token.getType() == TokenType.DIV) {
             match(TokenType.DIV);
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void Fator(){
-        if(token.getType() == TokenType.ID){
+    private void Fator() {
+        if (token.getType() == TokenType.ID) {
             match(TokenType.ID);
             ChamadaFuncao();
-        }
-        else if(token.getType() == TokenType.INT_CONST){
+        } else if (token.getType() == TokenType.INT_CONST) {
             match(TokenType.INT_CONST);
-        }
-        else if(token.getType() == TokenType.FLOAT_CONST){
+        } else if (token.getType() == TokenType.FLOAT_CONST) {
             match(TokenType.FLOAT_CONST);
-        }
-        else if(token.getType() == TokenType.CHAR_LITERAL){
+        } else if (token.getType() == TokenType.CHAR_LITERAL) {
             match(TokenType.CHAR_LITERAL);
-        }
-        else if(token.getType() == TokenType.LBRACKET){
+        } else if (token.getType() == TokenType.LBRACKET) {
             match(TokenType.LBRACKET);
             Expr();
             match(TokenType.RBRACKET);
-        }
-        else{
+        } else {
             PrintError();
         }
     }
 
-    private void ChamadaFuncao(){
-        if(token.getType() == TokenType.LBRACKET){
+    private void ChamadaFuncao() {
+        if (token.getType() == TokenType.LBRACKET) {
             match(TokenType.LBRACKET);
             ListaArgs();
             match(TokenType.RBRACKET);
         }
     }
 
-    private void ListaArgs(){
-        if(token.getType() == TokenType.ID || token.getType() == TokenType.INT_CONST || token.getType() == TokenType.FLOAT_CONST || token.getType() == TokenType.CHAR_LITERAL){
+    private void ListaArgs() {
+        if (
+            token.getType() == TokenType.ID ||
+            token.getType() == TokenType.INT_CONST ||
+            token.getType() == TokenType.FLOAT_CONST ||
+            token.getType() == TokenType.CHAR_LITERAL
+        ) {
             Arg();
             ListaArgs2();
         }
     }
 
-    private void ListaArgs2(){
-        if(token.getType() == TokenType.COLON){
-            match(TokenType.COLON);
+    private void ListaArgs2() {
+        if (token.getType() == TokenType.COMMA) {
+            match(TokenType.COMMA);
             Arg();
             ListaArgs2();
         }
     }
-    
-    private void Arg(){
-        if(token.getType() == TokenType.ID){
+
+    private void Arg() {
+        if (token.getType() == TokenType.ID) {
             match(TokenType.ID);
             ChamadaFuncao();
-        }
-        else if(token.getType() == TokenType.INT_CONST){
+        } else if (token.getType() == TokenType.INT_CONST) {
             match(TokenType.INT_CONST);
-        }
-        else if(token.getType() == TokenType.FLOAT_CONST){
+        } else if (token.getType() == TokenType.FLOAT_CONST) {
             match(TokenType.FLOAT_CONST);
-        }
-        else if(token.getType() == TokenType.CHAR_LITERAL){
+        } else if (token.getType() == TokenType.CHAR_LITERAL) {
             match(TokenType.CHAR_LITERAL);
-        }
-        else{
+        } else {
             PrintError();
         }
     }
